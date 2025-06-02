ca5696927e73c7c30e19db7bc82b344e003d6701
bao <baonguyendng123@gmail.com>
2025-06-02 22:39:31 +0700

    tao file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation ("com.google.android.material:material:1.6.0")
 }
---
+++ app/src/main/java/com/example/myapplication/data/entity/test.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.data.entity;
+
+public class test {
+}
