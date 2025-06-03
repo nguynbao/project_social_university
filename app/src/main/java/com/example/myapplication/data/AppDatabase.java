7f36ab03fc809490360160c528c4f9a8792e448c
bao <baonguyendng123@gmail.com>
2025-06-03 15:05:40 +0700

    bao

--- app/src/main/java/com/example/myapplication/data/AppDatabase.java
+++ app/src/main/java/com/example/myapplication/data/AppDatabase.java
@@ -6,14 +6,31 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.myapplication.data.dao.RegisteredActivityDao;
 import com.example.myapplication.data.dao.UserDao;
+import com.example.myapplication.data.entity.RegisteredActivity;
 import com.example.myapplication.data.entity.User;
+import com.example.myapplication.data.dao.CommentDao;
+import com.example.myapplication.data.entity.Comment;
+import com.example.myapplication.data.dao.PostDao;
+import com.example.myapplication.data.entity.Post;
+import com.example.myapplication.data.dao.DocumentDao;
+import com.example.myapplication.data.entity.Document;
+import com.example.myapplication.data.dao.LikeDao;
+import com.example.myapplication.data.entity.Like;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Comment.class, Post.class, Document.class, Like.class, RegisteredActivity.class},  version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     private static volatile AppDatabase INSTANCE;
 
     public abstract UserDao userDao();
+    public abstract CommentDao commentDao();
+    public abstract PostDao postDao();
+    public abstract DocumentDao documentDao();
+    public abstract LikeDao likeDao();
+    public abstract RegisteredActivityDao registeredActivityDao();
+
+
     // Thêm các DAO khác nếu có
 
     public static AppDatabase getDatabase(final Context context) {
