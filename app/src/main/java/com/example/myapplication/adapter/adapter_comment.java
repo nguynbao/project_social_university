074f2697fb19ff212aa857fd7e8383ca34591b87
danh_thanh <tinhoc7649@gmail.com>
2025-06-04 19:16:22 +0700

    danh

---
+++ app/src/main/java/com/example/myapplication/adapter/adapter_comment.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class adapter_comment extends RecyclerView.Adapter<adapter_comment.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adapter_comment.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
